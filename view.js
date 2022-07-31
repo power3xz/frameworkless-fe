e12c62fd8f38959adcaf5fdff468562efc4a0ac6
devj <power3xz@gmail.com>
2022-07-31 21:30:08 +0900

    view.js 수정

--- view.js
+++ view.js
@@ -1,42 +1,16 @@
-const getTodoElement = (todo) => {
-  const { text, completed } = todo;
-  return `<li ${completed ? 'class="completed"' : ""}>
-    <div class="view">
-      <input
-        ${completed ? "checked" : ""}
-        class="toggle"
-        type="checkbox">
-      <label>${text}</label>
-      <button class="destroy"></button>
-    </div>
-    <input class="edit" value="${text}" />
-  </li>`;
-};
+import todosView from "./todos.js";
 
-const getTodoCount = (todos) => {
-  const notCompleted = todos.filter((todo) => !todo.completed);
-  const { length } = notCompleted;
-  if (length === 1) {
-    return `1 Item left`;
-  }
-  return `${length} Items left`;
-};
+import counterView from "./counter.js";
+import filtersView from "./filters.js";
+
 export default (targetElement, state) => {
-  const { currentFilter, todos } = state;
-
   const element = targetElement.cloneNode(true);
-  const list = element.querySelector(".todo-list");
+  const list = element.querySelector("todo-list");
   const counter = element.querySelector(".todo-count");
   const filters = element.querySelector(".filters");
 
-  list.innerHTML = todos.map(getTodoElement).join("");
-  counter.textContent = getTodoCount(todos);
-  Array.from(filters.querySelector("li a")).forEach((a) => {
-    if (a.textContent === currentFilter) {
-      a.classList.add("selected");
-    } else {
-      a.classList.remove("selected");
-    }
-  });
+  list.replaceWith(todosView(list, state));
+  counter.replaceWith(counterView(counter, state));
+  filters.replaceWith(filtersView(filters, state));
   return element;
 };
