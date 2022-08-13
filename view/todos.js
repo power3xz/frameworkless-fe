6fa79b0795a0109d0c72d2ef8fd7b58a7939f3a2
devj <power3xz@gmail.com>
2022-08-13 17:35:40 +0900

    todo-item 템플릿 사용으로 변경

--- view/todos.js
+++ view/todos.js
@@ -1,21 +1,30 @@
+let template;
+
+const createNewNode = () => {
+  if (!template) {
+    template = document.getElementById("todo-item");
+  }
+  return template.content.firstElementChild.cloneNode(true);
+};
+
 const getTodoElement = (todo) => {
   const { text, completed } = todo;
-  return `<li ${completed ? 'class="completed"' : ""}>
-      <div class="view">
-        <input
-          ${completed ? "checked" : ""}
-          class="toggle"
-          type="checkbox" />
-        <label>${text}</label>
-        <button class="destroy"></button>
-      </div>
-      <input class="edit" value="${text}" />
-    </li>`;
+  const element = createNewNode();
+
+  element.querySelector("input.edit").value = text;
+  element.querySelector("label").textContent = text;
+  if (completed) {
+    element.classList.add("completed");
+    element.querySelector("input.toggle").checked = true;
+  }
+  return element;
 };
 
 export default (targetElement, { todos }) => {
   const newTodoList = targetElement.cloneNode(true);
-  const todosElements = todos.map(getTodoElement).join("");
-  newTodoList.innerHTML = todosElements;
+  newTodoList.innerHTML = "";
+  todos.map(getTodoElement).forEach((element) => {
+    newTodoList.appendChild(element);
+  });
   return newTodoList;
 };
